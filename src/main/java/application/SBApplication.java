72c9ef478c04e6b8fac0c3ef41916bbbf239086a
Microclimate User <microclimate.user@localhost>
2019-07-05 13:45:38 +0000

    Updating Eureka Changes w r to Microclimate

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -3,8 +3,10 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 @ComponentScan(basePackages = {"application", "com.ibm.javametrics.spring"})
 public class SBApplication {
 
